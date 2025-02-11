b96c6a46b5cd3f1a63bdf640628ce9374437fa76
Tu Nombre <you@example.com>
2025-02-11 18:14:57 +0100

    cambios clase martes

--- DarioSymfonyProyecto/public/script/scriptCancion.js
+++ DarioSymfonyProyecto/public/script/scriptCancion.js
@@ -1,18 +1,54 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
 let playAudio = document.getElementById('audioPlayer');
 let footer = document.querySelector('footer');
 let navCanciones = document.getElementById('navCanciones');
 let navPlaylist = document.getElementById('navPlaylist');
 
 
-navCanciones.addEventListener('click', function(e){
-    e.preventDefault(); //cuando pulsemos la pagina no hace la interaccion de recargar
-    document.getElementById('contenidoPrincipal').innerHTML = "";
-
-
+function verPlaylist(){
     let parrafo = document.createElement('h1');
-    parrafo.textContent = "Tus Canciones";
-    parrafo.classList.add('tituloContenidoPrincipal');
-    document.getElementById('contenidoPrincipal').appendChild(parrafo);
+    parrafo.textContent = "Tus Playlist";
+    parrafo.classList.add('tituloContenido');
+    document.getElementById('contenidoPlaylist').appendChild(parrafo);
+
+
+    fetch('/playlist/mostrarPlaylist')
+    .then(response => response.json())
+    .then(datosPlaylist => {
+        let playlists = datosPlaylist;
+        let grupoPlaylists = document.createElement('div');
+        grupoPlaylists.classList.add('playlists');
+
+        for (let p of playlists) {
+            let img = document.createElement('img');
+            img.src = './imagenes/coverPlaylist.png';
+            img.classList.add('imgMusica');
+
+
+            let divPlaylist = document.createElement('div');
+            let nombrePlaylist = document.createElement('p');
+            nombrePlaylist.classList.add('textoCancion');
+            nombrePlaylist.textContent = p.nombre;
+
+            divPlaylist.appendChild(img);
+            divPlaylist.appendChild(nombrePlaylist);
+            grupoPlaylists.appendChild(divPlaylist);
+
+
+            document.getElementById('contenidoPlaylist').appendChild(grupoPlaylists);
+
+        }
+    })
+}
+
+
+function verCanciones(){
+    let parrafo2 = document.createElement('h1');
+    parrafo2.textContent = "Tus Canciones";
+    parrafo2.classList.add('tituloContenido');
+    document.getElementById('contenidoCanciones').appendChild(parrafo2);
 
 
     fetch('/cancion/mostrarCanciones')
@@ -57,7 +93,7 @@ navCanciones.addEventListener('click', function(e){
             divCancion.appendChild(img);
             divCancion.appendChild(titulo);
             grupoCanciones.appendChild(divCancion);
-            document.getElementById('contenidoPrincipal').appendChild(grupoCanciones);
+            document.getElementById('contenidoCanciones').appendChild(grupoCanciones);
 
             divCancion.addEventListener('click', () => {
                 tituloFooter.textContent = cancion.titulo;
@@ -72,46 +108,10 @@ navCanciones.addEventListener('click', function(e){
             });
         }
     })
-})
+}
+
+verPlaylist();
+verCanciones();
 
 
-
-navPlaylist.addEventListener('click', function(e){
-    e.preventDefault(); //cuando pulsemos la pagina no hace la interaccion de recargar
-    document.getElementById('contenidoPrincipal').innerHTML = "";
-
-
-    let parrafo = document.createElement('h1');
-    parrafo.textContent = "Tus Playlist";
-    parrafo.classList.add('tituloContenidoPrincipal');
-    document.getElementById('contenidoPrincipal').appendChild(parrafo);
-
-
-    fetch('/playlist/mostrarPlaylist')
-    .then(response => response.json())
-    .then(datosPlaylist => {
-        let playlists = datosPlaylist;
-        let grupoPlaylists = document.createElement('div');
-        grupoPlaylists.classList.add('playlists');
-
-        for (let p of playlists) {
-            let img = document.createElement('img');
-            img.src = './imagenes/coverPlaylist.png';
-            img.classList.add('imgMusica');
-
-
-            let divPlaylist = document.createElement('div');
-            let nombrePlaylist = document.createElement('p');
-            nombrePlaylist.classList.add('textoCancion');
-            nombrePlaylist.textContent = p.nombre;
-
-            divPlaylist.appendChild(img);
-            divPlaylist.appendChild(nombrePlaylist);
-            grupoPlaylists.appendChild(divPlaylist);
-
-
-            document.getElementById('contenidoPrincipal').appendChild(grupoPlaylists);
-
-        }
-    })
 })
---
+++ DarioSymfonyProyecto/public/script/scriptPlaylist.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let playAudio = document.getElementById('audioPlayer');
+    let footer = document.querySelector('footer');
+    let navCanciones = document.getElementById('navCanciones');
+    let navPlaylist = document.getElementById('navPlaylist');
+    
+    
+    
+        let parrafo = document.createElement('h1');
+        parrafo.textContent = "Tus Playlist";
+        parrafo.classList.add('tituloContenido');
+        document.getElementById('contenidoPlaylist').appendChild(parrafo);
+    
+    
+        fetch('/playlist/mostrarPlaylist')
+        .then(response => response.json())
+        .then(datosPlaylist => {
+            let playlists = datosPlaylist;
+            let grupoPlaylists = document.createElement('div');
+            grupoPlaylists.classList.add('playlists');
+    
+            for (let p of playlists) {
+                let img = document.createElement('img');
+                img.src = './imagenes/coverPlaylist.png';
+                img.classList.add('imgMusica');
+    
+    
+                let divPlaylist = document.createElement('div');
+                let nombrePlaylist = document.createElement('p');
+                nombrePlaylist.classList.add('textoCancion');
+                nombrePlaylist.textContent = p.nombre;
+    
+                divPlaylist.appendChild(img);
+                divPlaylist.appendChild(nombrePlaylist);
+                grupoPlaylists.appendChild(divPlaylist);
+    
+    
+                document.getElementById('contenidoPlaylist').appendChild(grupoPlaylists);
+    
+            }
+        })
+
+    })
